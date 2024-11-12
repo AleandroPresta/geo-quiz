ce040220697c57462b2f3a8bf53d58ccc7af48bf
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-11-12 12:15:33 +0100

    Removed footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QuizComponent } from "./quiz/quiz.component";
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, QuizComponent, FooterComponent],
+  imports: [RouterOutlet, QuizComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
